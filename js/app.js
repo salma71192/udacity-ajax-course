3ae7e19d7afb715f41f97cd370f4ae7e181166b9
Salma Mohammed <sm.71175@gmail.com>
2018-02-16 22:20:24 +0200

    Feat: connent to unsplash API.

--- js/app.js
+++ js/app.js
@@ -9,4 +9,13 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
     });
+
+    function addImage(){}
+    searchedForText = 'hippos';
+    const unsplashRequest = new XMLHttpRequest();
+
+    unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+    unsplashRequest.onload = addImage;
+    unsplashRequest.setRequestHeader('Authorization', 'Client-ID 2f8dd55983a8084e313a47fbbdee8bb04204c717818973b423e09ac7786083b4');
+    unsplashRequest.send();
 })();
